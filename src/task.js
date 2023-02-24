cce391d6413a13005f60ecfc234a8014d317c74a
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-02-23 21:14:03 -0500

    Implement datepicker

--- src/task.js
+++ src/task.js
@@ -1,3 +1,6 @@
+import { Datepicker } from "vanillajs-datepicker";
+import "vanillajs-datepicker/css/datepicker.css";
+
 const taskContainer = document.querySelector(".task-container");
 
 export default class Task {
@@ -11,30 +14,47 @@ export default class Task {
     // Create DOM nodes
     const taskDiv = document.createElement("div");
     const inputTask = document.createElement("input");
+    const dateTask = document.createElement("input");
+    const priorityTask = document.createElement("div");
     const delTask = document.createElement("button");
     const checkTask = document.createElement("input");
 
     // Qualify DOM nodes
     inputTask.className = "input-task";
     checkTask.className = "check-task";
+    dateTask.className = "date-task";
     taskDiv.className = "task";
     checkTask.type = "checkbox";
     inputTask.value = this.name;
+    dateTask.textContent = "Date";
     delTask.textContent = "X";
+
+    dateTask.value = "Due Date";
+
+    // Date
+
+    dateTask.addEventListener("click", () => {
+      // dateTask.type = "input";
+      const datePicker = new Datepicker(dateTask);
+      console.log(datePicker);
+    });
 
     // State persistence if previously marked Done
     if (this.done === true) {
       inputTask.disabled = true;
       checkTask.checked = true;
+      dateTask.disabled = true;
     }
 
     // Checkbox event handler
     checkTask.addEventListener("click", () => {
       if (this.done === false) {
         inputTask.disabled = true;
+        dateTask.disabled = true;
         this.done = true;
       } else {
         inputTask.disabled = false;
+        dateTask.disabled = false;
         this.done = false;
       }
     });
@@ -47,6 +67,7 @@ export default class Task {
 
     // Compose task DOM element
     taskDiv.appendChild(inputTask);
+    taskDiv.appendChild(dateTask);
     taskDiv.appendChild(delTask);
     taskDiv.appendChild(checkTask);
 
